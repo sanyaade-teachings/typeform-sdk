3b8e63d930f3da96f6b3278bf1ceccfc6b3f6fc9
Marc <marcbaro22@gmail.com>
2015-05-22 21:55:12 +0200

    Implementation of the https post methods + functional statementField model

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
         src: ['Gruntfile.js']
       },
       js: {
-        src: ['*.js']
+        src: ['lib/*.js', 'lib/**/*.js']
       },
       test: {
         src: ['test/**/*.js']
--- test/test.js
+++ test/test.js
@@ -1,20 +1,60 @@
 'use strict';
 /*jshint expr: true*/
-var config + require('./config');
+var config = require('./config');
 var expect = require('chai').expect;
 var TypeForm = require('../');
+var Form = require('../lib/models/form');
+var StatementField = require('../lib/models/statementField');
+var MultipleChoiceField = require('../lib/models/multipleChoiceField');
 
 describe('typeform-sdk node module', function () {
   it('must have at least one test', function () {
     var t = new TypeForm();
     expect(t).to.exist;
+
   });
-  it('retrieves information from the api', function (done) {
+  it('retrieves correct information from the api', function (done) {
     var t = new TypeForm(config.apiKey, config.proxy);
     t.info(function(res,err){
-      console.log('hei' + JSON.stringify(res));
+      if(err){
+        console.error(err);
+      }
       expect(res).to.exist;
+      expect(res.name).to.be.equal('Typeform I/O Build API');
       done();
     });
   });
+  it('can create a correct form with a statementField', function (done) {
+    var t = new TypeForm(config);
+    var form = new Form('MyForm', 21, 'http://example.com');
+    form.fields.push(new StatementField('This is an awesome form!'));
+    t.createForm(form, function(res,err){
+      if(err){
+        console.error(err);
+      }
+      expect(res).to.exist;
+      expect(res.title).to.be.equal('MyForm');
+      expect(res.design_id).to.be.equal(21);
+      expect(res.fields[0].question).to.be.equal('This is an awesome form!');
+      done();
+    });
+  });
+  it('can create a correct form with a MultipleChoiceField', function (done) {
+      var t = new TypeForm(config);
+      var form = new Form('MyForm', 21, 'http://example.com');
+      form.fields.push(new StatementField('This is an awesome form!'));
+      form.fields.push(new MultipleChoiceField('Choose your weapon:', ['sword', 'staff', 'shield', 'keyblade']));
+      t.createForm(form, function(res,err){
+        if(err){
+          console.error(err);
+        }
+        expect(res).to.exist;
+        expect(res.title).to.be.equal('MyForm');
+        expect(res.design_id).to.be.equal(21);
+        expect(res.fields[0].question).to.be.equal('This is an awesome form!');
+        expect(res.fields[1].question).to.be.equal('Choose your weapon:');
+        console.log(res.links.form_render.get);
+        done();
+      });
+    });
 });
